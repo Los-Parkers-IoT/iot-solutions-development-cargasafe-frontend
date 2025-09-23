f8215b47d3ef972b56bbe6fc05a3edf0f33cd261
Alessandro <u202113640@upc.edu.pe>
2025-09-23 01:03:17 -0500

    feat: First commit. Project Setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
