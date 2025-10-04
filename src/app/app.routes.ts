7026161ab82fb39e5a781a056f42dbe8cdd7c285
SgtGarro <marcelogarro137@gmail.com>
2025-10-03 21:02:30 -0500

    feat(shared): implement root layout and routing system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { RootLayout } from './shared/presentation/layout/root-layout/root-layout';
+import { PageNotFound } from './shared/presentation/views/page-not-found/page-not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: RootLayout,
+    children: [
+      {
+        path: '**',
+        component: PageNotFound,
+      },
+    ],
+  },
+];
