fe46756d0d9677cf978e5dfe55146b5faef74cd3
Nicolas <122683587+nicxlxs@users.noreply.github.com>
2025-10-08 02:02:18 -0500

    feat(env): add base environment files and production config

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:3000'  // tu JSON-Server
+  //apiUrl: 'https://fake-api-murex-one.vercel.app'  // tu JSON-Server
+};
