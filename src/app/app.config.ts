37cc80c0510f5b638b1cf1ce4b9356d58cfd6c0d
Nicolas <122683587+nicxlxs@users.noreply.github.com>
2025-10-08 04:36:29 -0500

    feat(shared-layout): add nav links to Vehicles and Devices management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,15 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptorsFromDi()), // <= AQUI
+
   ]
 };
--- src/app/shared/presentation/layout/root-layout/root-layout.ts
+++ src/app/shared/presentation/layout/root-layout/root-layout.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './root-layout.html',
   styleUrl: './root-layout.css',
 })
