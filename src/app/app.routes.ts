ba3f41542584639bd23c73eb034396c910b8c6de
Nicolas <122683587+nicxlxs@users.noreply.github.com>
2025-10-08 04:26:23 -0500

    feat(routes): add/modify Fleet routes and guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,32 @@
 import { Routes } from '@angular/router';
 import { RootLayout } from './shared/presentation/layout/root-layout/root-layout';
 import { PageNotFound } from './shared/presentation/views/page-not-found/page-not-found';
+import {VehicleManagementComponent} from './fleet/presentation/pages/vehicle-management/vehicle-management';
+import {DeviceManagementComponent} from './fleet/presentation/pages/device-management/device-management';
+import {VehicleDetailPageComponent} from './fleet/presentation/pages/vehicle-detail-page/vehicle-detail-page';
+import {DeviceDetailPageComponent} from './fleet/presentation/pages/device-detail-page/device-detail-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: RootLayout,
     children: [
+      { path: '', pathMatch: 'full', redirectTo: 'fleet/vehicles' },
+
       {
-        path: '**',
-        component: PageNotFound,
+        path: 'fleet',
+        children: [
+          { path: 'vehicles', component: VehicleManagementComponent },
+          { path: 'devices', component: DeviceManagementComponent },
+
+          { path: 'vehicles/:id', component: VehicleDetailPageComponent },
+          { path: 'devices/:id', component: DeviceDetailPageComponent },
+
+          { path: '', pathMatch: 'full', redirectTo: 'vehicles' },
+        ],
       },
+
+      { path: '**', component: PageNotFound },
     ],
   },
 ];
