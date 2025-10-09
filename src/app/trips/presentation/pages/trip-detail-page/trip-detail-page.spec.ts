88734ae3907ecf3b38575fff0d1533e2f72811e5
SgtGarro <marcelogarro137@gmail.com>
2025-10-09 04:50:01 -0500

    chore(db): add trips data

---
+++ src/app/trips/presentation/pages/trip-detail-page/trip-detail-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TripDetailPage } from './trip-detail-page';
+
+describe('TripDetailPage', () => {
+  let component: TripDetailPage;
+  let fixture: ComponentFixture<TripDetailPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [TripDetailPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TripDetailPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/trips/presentation/pages/trip-detail-page/trip-detail-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-detail-page',
+  imports: [],
+  templateUrl: './trip-detail-page.html',
+  styleUrl: './trip-detail-page.css'
+})
+export class TripDetailPage {
+
+}
