caa30a3a606541eb9d8c90c9d4305aaee8664cc7
nicxlxs <nicolas.vera.nunez.2004@gmail.com>
2025-11-13 12:55:12 -0500

    fix(environment): change link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://iot-solutions-development-cargasafe.onrender.com/api/v1'
-  //apiUrl: 'https://fake-api-murex-one.vercel.app'  // tu servidor real
-};
+  baseUrl: 'https://iot-solutions-development-cargasafe.onrender.com/api/v1',
+  tripsEndpointPath: '/trips',
+  deliveryOrdersEndpointPath: '/delivery-orders',
+  alertsEndpointPath: '/alerts',
+  googleMapsApiKey: 'AIzaSyDEpu21mrXEAewZHnvMxOfR3Nj3VLZLECk',
+} as const;
