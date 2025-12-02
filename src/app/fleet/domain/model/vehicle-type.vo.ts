721ab8ec4e2e1cd4e865a0481e289d972bfc2e12
Nicolas <122683587+nicxlxs@users.noreply.github.com>
2025-12-02 01:24:07 -0500

    feat(fleet-domain): model vehicle domain and repositories

---
+++ src/app/fleet/domain/model/vehicle-type.vo.ts
@@ -0,0 +1,13 @@
+export enum VehicleType {
+  TRUCK = 'TRUCK',
+  VAN = 'VAN',
+  CAR = 'CAR',
+  MOTORCYCLE = 'MOTORCYCLE',
+}
+
+export const ALL_VEHICLE_TYPES: VehicleType[] = [
+  VehicleType.TRUCK,
+  VehicleType.VAN,
+  VehicleType.CAR,
+  VehicleType.MOTORCYCLE,
+];
